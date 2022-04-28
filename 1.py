26543133bcae244c791ea6f879d9914997113c9a
Shashank-Handa <handa.shashank.36@gmail.com>
2022-04-28 08:38:44 +0530

    working

--- 1.py
+++ 1.py
@@ -2,14 +2,14 @@
 import socket
 import sys
 
-#import psutil
+import psutil
 import pyshark
 import os
 import subprocess
 
-ipAddr="10.0.2.5"
-interface_name='any'
-filter_string='src host 10.0.2.5 or dst host 10.0.2.5'
+ipAddr="10.0.0.13"
+interface_name='Wi-Fi'
+filter_string='src host 10.0.0.13 or dst host 10.0.0.13'
 
 """def get_pid(port):
     connections = psutil.net_connections()
@@ -20,16 +20,17 @@ filter_string='src host 10.0.2.5 or dst host 10.0.2.5'
             return con.pid
     return -1"""
 
-"""def get_pid(port):
+def get_pid(port):
+    port=int(port)
     print("called")
     for process in psutil.process_iter():
-        for conns in process.connections(kind='inet'):
+        for conns in process.connections(kind='all'):
             if conns.laddr.port == port:
                 print(process.name())
-                return process.name()"""
+                return process.name()
 
 
-def get_pid(port):
+"""def get_pid(port):
     process=subprocess.Popen(["lsof","-i",":{0}".format(port)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     stdout, stderrr = process.communicate()
     for process in str(stdout.decode("utf-8")).split("/n")[1:]:
@@ -38,7 +39,7 @@ def get_pid(port):
             continue
         print(data)
         return data[0]
-
+"""
 capture=pyshark.LiveCapture(
     bpf_filter=filter_string,
     interface=interface_name
@@ -52,20 +53,24 @@ upStream={}
 
 if(len(capture)>0):
     for packet in capture:
-        protocol=packet.transport_layer
-        if(packet.ip.src==ipAddr):
-            print("up")
-            if(packet[protocol].srcport in upStream):
-                upStream[packet[protocol].srcport][0]+=int(packet.length)
-            else:
-                upStream[packet[protocol].srcport]=[int(packet.length)]
-                upStream[packet[protocol].srcport].append(get_pid(packet[protocol].srcport))
-        if(packet.ip.dst==ipAddr):
-            print("down")
-            if(packet[protocol].dstport in downStream):
-                downStream[packet[protocol].dstport][0]+=int(packet.length)
-            else:
-                downStream[packet[protocol].dstport]=[int(packet.length)]
-                downStream[packet[protocol].dstport].append(get_pid(packet[protocol].dstport))
+        try:
+            protocol=packet.transport_layer
+            if(packet.ip.src==ipAddr):
+                print("up")
+                if(packet[protocol].srcport in upStream):
+                    upStream[packet[protocol].srcport][0]+=int(packet.length)
+                else:
+                    upStream[packet[protocol].srcport]=[int(packet.length)]
+                    upStream[packet[protocol].srcport].append(get_pid(packet[protocol].srcport))
+            if(packet.ip.dst==ipAddr):
+                print("down")
+                if(packet[protocol].dstport in downStream):
+                    downStream[packet[protocol].dstport][0]+=int(packet.length)
+                else:
+                    downStream[packet[protocol].dstport]=[int(packet.length)]
+                    downStream[packet[protocol].dstport].append(get_pid(packet[protocol].dstport))
+        except AttributeError:
+            continue
     print(downStream)
     print(upStream)
+    
